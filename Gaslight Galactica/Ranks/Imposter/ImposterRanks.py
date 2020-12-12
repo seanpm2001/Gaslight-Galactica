ef91580c4b14c08ebdb42e8db7d0beb794c2083c
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-11 19:53:25 -0800

    ImposterRanks.py
    The imposter ranks file for Gaslight Galactica.

---
+++ Gaslight Galactica/Ranks/Imposter/ImposterRanks.py
@@ -0,0 +1,45 @@
+# Start of script
+'''
+The imposter rank script for Gaslight Galactica
+'''
+def imposterRankList():
+	rank1 = bool("true")
+	rank1WinCount = int(0, 3)
+	rank2 = bool("false")
+	rank2WinCount = int(4, 10)
+	rank3 = bool("false")
+	rank3WinCount = int(11, 25)
+	rank4 = bool("false")
+	rank4WinCount = int(26, 50)
+	rank5 = bool("false")
+	rank5WinCount = int(51, 100)
+	rank6 = bool("false")
+	rank6WinCount = int(101, 256)
+	rank7 = bool("false")
+	rank7WinCount = int(257, 1000)
+	rank8 = bool("false")
+	rank8WinCount = int(1001, 5000)
+	rank9 = bool("false")
+	rank9WinCount = int(5001, 20000)
+	rank10 = bool("false")
+	rank10WinCount = int(20001, 50000)
+	# Rank adjustments need to be made
+# Import section
+import random() # Not needed, I just wanted to import something
+# Definition of functions section
+print ("Gaslight Galactica imposter ranks")
+# End of script (quit condition)
+noMore = input("Press [ENTER] to quit")
+print ("This program should now be closed. If the window is still open, press the close button. If this doesn't work, try ending the task/process with a task manager/process manager")
+"""
+File info
+File type: Python script file (*.py)
+File version: 1 (Friday, December 11th 2020 at 7:53 pm)
+Line count (including blank lines and compiler line): 46
+"""
+# Divider
+'''
+ToDo:
+adjust ranks
+'''
+# End of script
