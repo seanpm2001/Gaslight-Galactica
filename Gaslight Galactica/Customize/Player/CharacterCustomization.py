00c176f4d8cfd91a3e8cc9d225e59fb159523111
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-11 19:34:16 -0800

    CharacterCustomization.py
    The character customization script for Gaslight Galactica.

---
+++ Gaslight Galactica/Customize/Player/CharacterCustomization.py
@@ -0,0 +1,25 @@
+# Start of script
+def hatSlot():
+	# Coming soon
+def eyewearSlot():
+	# Coming soon
+# Main output
+print ("Gaslight Galactica character customization")
+print ("NOT READY YET")
+# End of script (quit condition)
+noMore = input("Press [ENTER] to quit")
+print ("This program should now be closed. If the window is still open, press the close button. If this doesn't work, try ending the task/process with a task manager/process manager")
+"""
+File info
+File type: Python script file (*.py)
+File version: 1 (Friday, December 11th 2020 at 7:34 pm)
+Line count (including blank lines and compiler line): 26
+"""
+# Divider
+'''
+ToDo:
+add more data
+Add hats
+Add eyewear
+'''
+# End of script
