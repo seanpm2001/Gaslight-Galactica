0cc9b123f409f6fefeef7897ed54f7c37d0ccde1
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-11 19:45:33 -0800

    CreateServer.py
    The server creation tool script for Gaslight Galactica.

---
+++ Gaslight Galactica/Servers/Server_Creation/CreateServer.py
@@ -0,0 +1,39 @@
+# Start of script
+'''
+The server creation script for Gaslight Galactica
+'''
+# Import section
+import random() # Not needed, I just wanted to import something
+# Definition of functions section
+def lan():
+	print ("Dictionary definition of LAN: Local Area Network")
+	print ("Merged with WLAN (Wireless Local Area Network) for simplicity")
+def global():
+	print ("Dictionary definition for global servers: a server that can be played worldwide, and not locked to a specific country, state, or continent")
+def createServer():
+	print ("Script coming soon")
+def serverTypes():
+	print ("Server types")
+	print ("WLAN/LAN")
+	print ("City only") # Also for villages, towns, and other related civilization types, city is just a general term
+	print ("State only")
+	print ("Country only")
+	print ("Continent only")
+	print ("Global")
+print ("Gaslight Galactica server creation")
+print (serverTypes())
+# End of script (quit condition)
+noMore = input("Press [ENTER] to quit")
+print ("This program should now be closed. If the window is still open, press the close button. If this doesn't work, try ending the task/process with a task manager/process manager")
+"""
+File info
+File type: Python script file (*.py)
+File version: 1 (Friday, December 11th 2020 at 7:45 pm)
+Line count (including blank lines and compiler line): 40
+"""
+# Divider
+'''
+ToDo:
+Improve the script
+'''
+# End of script
